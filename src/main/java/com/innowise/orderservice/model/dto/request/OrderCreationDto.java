5305b18b894b4656f73f3bfa41decfd7855c767d
BadzeiPavel <pavelbadzei@gmail.com>
2026-06-09 15:30:27 +0300

    fix sonar comments

--- src/main/java/com/innowise/orderservice/model/dto/request/OrderCreationDto.java
+++ src/main/java/com/innowise/orderservice/model/dto/request/OrderCreationDto.java
@@ -3,7 +3,6 @@ package com.innowise.orderservice.model.dto.request;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
-import java.util.UUID;
 
 public record OrderCreationDto(
     @NotNull
